a2fa109158ed2e5fbeaac6217a988e5cd3185ad3
Dani <creepycereals@gmail.com>
2014-08-12 21:39:02 +0200

    Update Sprite.js

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,12 +1,12 @@
 (function(){
 
-	function Sprite(size){
+	function Sprite(size){ // Constructor
 		this.size = size;
 
 	}
 
 	Sprite.prototype = {
-		render: function(context){ // The context passed has been translated to the position where the object needs to be drawn, that's why fillRect() takes (0, 0, ...) as argument. 
+		render: function(context){ // context has been translated to the position where the object needs to be drawn, that's why fillRect() takes (0, 0, ...) as argument. 
 
 			context.fillRect(0, 0, this.size[0], this.size[1]);
 		}
